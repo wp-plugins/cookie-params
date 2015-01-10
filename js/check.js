4dd4e19d990886ef7762c6fa62623a1ae0e57938
hypedtext <hypedtext@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-10 20:12:03 +0000

    Bug fixes
    git-svn-id: https://plugins.svn.wordpress.org/cookie-params/trunk@1064904 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/check.js
+++
@@ -1,6 +0,0 @@
-jQuery( document ).ready(function() {
-	var data = { action: 'debug' }; 
-	jQuery.post( check.ajaxurl, data, function(response) {
-		console.log( 'Got this from the server: ' + response );
-	});
-});
